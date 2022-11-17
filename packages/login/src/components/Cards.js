dabf2258e45dbcfbc269959ec079b01e93535b55
Deepa Thiagarajan <deepa.name@gmail.com>
2022-11-16 16:46:32 -0800

    adding login app as a package and scaffolding for other packages

---
+++ packages/login/src/components/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from "react-router-dom"
+
+const Cards = () => {
+    return (
+        <section>
+            <h1>Cards Page</h1>
+            <br />
+            <p>You must have been assigned an Cards role.</p>
+            <div className="flexGrow">
+                <Link to="/">Home</Link>
+            </div>
+        </section>
+    )
+}
+
+export default Cards
